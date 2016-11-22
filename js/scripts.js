2fa11f5de303196d5f5a6446448f348f48a50961
hayleygay <hgay1@vcd-ch204-18.eastern.ewu.edu>
2016-11-22 13:42:48 -0800

    Version 1
    Updated img files, added “full size” and “thumb” folders

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,14 @@
-// My Scripts
+// RANDOMIZE HEADER
+
+// Store & Generate Number
+var maxRand = 3;
+var numRand = Math.floor( Math.random() * maxRand );
+//console.log(numRand);
+
+// Store images
+var img0 = 'http://unsplash.it/200/200';
+var img1 = 'http://unsplash.it/300/300';
+var img2 = 'http://unsplash.it/400/400';
+
+// Show Images
+$('header img').attr('src', eval('img' + numRand) );
